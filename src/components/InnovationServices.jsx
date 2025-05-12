63d744023a6e2e7e5600f30f7333836d325a591e
Anaëlle Charlemoine <anaellecharlemoine@Anaelles-MacBook-Air.local>
2025-05-12 16:05:15 +0200

    Remove missing UI component imports for successful build

--- src/components/InnovationServices.jsx
+++ src/components/InnovationServices.jsx
@@ -1,5 +1,5 @@
-import { Card, CardContent } from "../ui/card";
-import { Button } from "../ui/button";
+import { Card, CardContent } from "../ui/div";
+import { Button } from "../ui/Button";
 
 export default function InnovationServices() {
   return (
